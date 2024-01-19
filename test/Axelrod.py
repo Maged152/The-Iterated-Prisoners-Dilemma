e343d20fecc914f180bc2cdc2d92a258bb6f09f1
Ahmed Maged <mageda9000@gmail.com>
2024-01-19 12:09:43 +0200

    add python test

---
+++ test/Axelrod.py
@@ -0,0 +1,38 @@
+import axelrod as axl
+
+def run_matches(strategies, rounds=100):
+    '''
+    winner, player_0_score, player_1_score
+    C,D,D,C ... #ply0,ply1,...
+    '''
+    file = open('python_results.txt','w')
+
+    for strategy1_idx in range(len(strategies)):
+        for strategy2_idx in range(strategy1_idx, len(strategies)):
+            players = (strategies[strategy1_idx], strategies[strategy2_idx])
+            match = axl.Match(players, turns=rounds)
+            
+            # Extract relevant information
+            interactions = match.play()
+            scores = match.final_score()
+            winner = match.winner()
+
+            file.write("%s,%d,%d\n" % (winner, scores[0], scores[1]))
+            moves = ",".join(["{},{}".format(move[0], move[1]) for move in interactions])
+            file.write("%s\n" % moves)   
+            
+
+    file.close()
+
+if __name__ == "__main__":
+    # Define the list of strategies you want to test
+    strategy_list = [axl.Cooperator(), axl.TitForTat()]
+    
+    
+    # Run matches
+    run_matches(strategy_list, 5)
+
+'''
+C:/Users/Acer/anaconda3/Scripts/activate
+conda activate base
+'''
